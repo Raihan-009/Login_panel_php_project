ae622b6a7fd7ad92692fc6facf0c75dbe3dc0e28
Raihan Islam <64744693+Raihan-009@users.noreply.github.com>
2022-01-01 19:29:59 +0600

    index file uploaded

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/all.css">
+</head>
+<body>
+    <section>
+        <h1>Admin Panel</h1>
+        <div class = "container">
+            <div class = "half-part">
+                <img src="login.jpeg" alt="">
+            </div>
+            <div class = "half-part">
+                <h1>Welcome Back!</h1>
+                <div class = "login_form">
+                    <div class = "input_icons">
+                        <i class="fas fa-user icon"></i>
+                        <input type="text" placeholder = "Username">
+                    </div> 
+                    <br>
+                    <div class="input_icons">
+                        <i class="fas fa-unlock-alt icon"></i>
+                        <input type="password" placeholder = Password>
+                    </div>
+
+                    <button type = "submit">Log In</button> <br>
+                </div>
+                <div class = "login_link">
+                    <a href="./">Forgot your password?</a>
+                </div>
+                <div class="stay_connect">
+                    <h3>Stay connected with us</h3>
+                    <i class="fab fa-facebook iconn"></i>
+                    <i class="fab fa-instagram-square iconn"></i>
+                    <i class="fab fa-linkedin iconn"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
